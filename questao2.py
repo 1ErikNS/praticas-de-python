ecb9d21945654e7b9ba6f6f87f5b0140ab9ed1d4
Erik Nascimento <eriknascimento035@gmail.com>
2025-04-02 03:11:19 -0300

    Questão

---
+++ questao2.py
@@ -0,0 +1,6 @@
+'''Implemente o jogo da adivinhação. Neste jogo, um número é sorteado e o usuário
+tenta adivinhar esse número por meio de uma sequência de palpites, ao passo que
+vai recebendo feedbacks indicando se o número alvo é "maior" ou "menor" que o
+último palpite dado. Para sortear um número inteiro, podemos usar a função
+randint do módulo random.'''
+
