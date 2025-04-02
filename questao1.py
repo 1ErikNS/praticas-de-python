97593eeaee66028f7a826f916ab25ddb2957a00b
Erik Nascimento <eriknascimento035@gmail.com>
2025-04-02 02:43:09 -0300

    Questão

---
+++ questao1.py
@@ -0,0 +1,9 @@
+'''Imaginemos que você é um topógrafo encarregado de calcular a altura de um
+prédio. Para isso, você possui um teodolito, um instrumento que permite medir
+ângulos verticais. Você posiciona o teodolito a uma distância conhecida do prédio e
+mede o ângulo de elevação até o topo do prédio. Com base nesses dados, você deve
+calcular a altura do prédio.
+Escreva um programa em Python que receba como entrada a distância do teodolito
+ao prédio e o ângulo de elevação até o topo do prédio. O programa deve calcular e
+exibir a altura do prédio.'''
+
